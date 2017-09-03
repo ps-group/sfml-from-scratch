6306b1228b013ad2f820bb5d3bfbe856dc0213f7
Sergey Shambir <sergey.shambir.auto@gmail.com>
2017-09-03 21:47:57 +0300

    Добавлены первые 3 примера базового курса по SFML

---
+++ 02/main.cpp
@@ -0,0 +1,37 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+int main()
+{
+    const sf::Vector2f speed = {20.f, 10.f};
+
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "title");
+    sf::Clock clock;
+
+    sf::CircleShape shape(40);
+    shape.setPosition({200, 120});
+    shape.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        const float dt = clock.restart().asSeconds();
+
+        sf::Vector2f position = shape.getPosition();
+        position += speed * dt;
+        shape.setPosition(position);
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
---
+++ 03/main.cpp
@@ -0,0 +1,51 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+int main()
+{
+    constexpr unsigned WINDOW_WIDTH = 800;
+    constexpr unsigned WINDOW_HEIGHT = 600;
+    constexpr float BALL_SIZE = 40;
+
+    sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "title");
+    sf::Clock clock;
+
+    sf::CircleShape shape(BALL_SIZE);
+    shape.setPosition({200, 120});
+    shape.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+
+    sf::Vector2f speed = {100.f, 100.f};
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        const float dt = clock.restart().asSeconds();
+
+        sf::Vector2f position = shape.getPosition();
+        position += speed * dt;
+
+        if ((position.x + 2 * BALL_SIZE >= WINDOW_WIDTH) || (position.x < 0))
+        {
+            speed.x = -speed.x;
+        }
+        if ((position.y + 2 * BALL_SIZE >= WINDOW_HEIGHT) || (position.y < 0))
+        {
+            speed.y = -speed.y;
+        }
+
+        shape.setPosition(position);
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
